0de47c34ea201a443f70dd9d190faf292690a032
sumanth <lsumanth08@gmail.com>
2025-07-02 10:26:21 +0530

    Remove batch files related to Git push operations

--- temp_git_push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add .
-git commit -m "Fix scheduling messages to use backend templates"
-git push origin main
-@echo on
--- update_and_push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add .
-git commit -m "Update API URLs to relative paths and configure CORS for mobile access"
-git push origin main
-@echo on
